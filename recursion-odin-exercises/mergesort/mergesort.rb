b99a3570103ccf0a0252ffb4ea1e41c13811bc95
jamcry <jamcry@hotmail.com>
2019-03-31 22:04:43 +0300

    Cleaned the code.

--- recursion-odin-exercises/mergesort/mergesort.rb
+++ recursion-odin-exercises/mergesort/mergesort.rb
@@ -1,6 +1,7 @@
 def mergesort(array)
     len = array.length
-    return array if len <= 1 # Base case
+    # Base case
+    return array if len <= 1
     
     # Divide array into smaller arrays and merge sort them
     a = mergesort(array[0...(len / 2)])
@@ -9,12 +10,15 @@ def mergesort(array)
 
     # Merge sub-arrays into new array "sorted"
     sorted = []
+
     until a.empty? || b.empty?
+        # add the smallest first value of a or b to sorted array
         sorted << (a[0] <= b[0] ? a.shift : b.shift)
     end
 
     sorted + a + b
 end
 
-my_a = [2, 3 ,5, 8, 12, 1]
-p mergesort(my_a)
+my_array = [2, 3 ,5, 8, 12, 1]
+puts "Input Array: #{my_array}"             # => Input Array: [2, 3, 5, 8, 12, 1]
+puts "Sorted Array: #{mergesort(my_array)}" # => Sorted Array: [1, 2, 3, 5, 8, 12]
