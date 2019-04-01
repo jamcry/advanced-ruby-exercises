3360127418db48ba2309665238cdbef4cf4e2035
jamcry <jamcry@hotmail.com>
2019-04-01 23:15:34 +0300

    Completed #append, #size, #head, #tail, #at(index), #to_s and #initialize functions.

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,9 +1,13 @@
 class LinkedList
-	def initialize
+	attr_reader :head
+	def initialize(value)
+    @head = Node.new(value, nil)
 	end
 
-	def append
-		# Add a new node to the end of the list
+	def append(value)
+    # Add a new node to the end of the list
+    # Append a new node to the end ( next node of tail) with specified value.
+		tail.next_node = Node.new(value, nil)
 	end
 
 	def preappend
@@ -11,19 +15,44 @@ class LinkedList
 	end
 
 	def size
-		# return the total number of nodes in the list
+    # return the total number of nodes in the list
+    current_node = @head
+    node_count = 0
+    while !current_node.nil?
+      node_count += 1
+      current_node = current_node.next_node
+    end
+    node_count
 	end
 
 	def head
-		# return the first node in the list
+    # return the first node in the list
+    #! this definition is unneccessary due to attr_reader
+    @head
 	end
 
 	def tail
-		# return the last node in the list
+    # return the last node in the list
+    current_node = @head
+    while !current_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+    current_node
 	end
 
 	def at(index)
-		# return the node at the given index
+    # return the node at the given index
+    current_node = @head
+    # return nil if index is out of range
+    if index > (size - 1)
+      nil
+    else
+      index_count = 0
+      (0...index).each do |i|
+        current_node = current_node.next_node
+      end
+      current_node.value
+    end
 	end
 
 	def pop
@@ -35,21 +64,44 @@ class LinkedList
 	end
 
 	def find(data)
-		# return the index of the node containing data or nil
+    # return the index of the node containing data or nil
+    #? can use (yield) in tail, inside while loop to use it
+    #? in find function.
+    #? tail {|val| val = data} break
 	end
 
 	def to_s
-		# Parse LinkedList to string # => ( data ) -> ( data ) -> ( data ) -> nil
+    # Parse LinkedList to string # => ( data ) -> ( data ) -> ( data ) -> nil
+    current_node = @head
+    parsed_list = ""
+    # Iterate until reaching the last (nil) node
+    until current_node.nil?
+      parsed_list += "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
+    end
+    parsed_list + "nil"
 	end
 	
-	end
 end
 
 class Node
 	attr_accessor :value, :next_node
 	
 	def initialize(value, next_node)
-		@value = nil
-		@next_node = nil
+		@value = value
+		@next_node = next_node
 	end
 end
+
+my_list = LinkedList.new(4)
+my_list.append(5)
+my_list.append(2)
+
+puts my_list.to_s
+puts "*Nodes     : #{my_list.size}"
+
+puts "*Head Value: #{my_list.head.value}"
+puts "*Tail Value: #{my_list.tail.value}"
+puts "*at (1)    : #{my_list.at(1)}"
+puts "*at (2)    : #{my_list.at(2)}"
+puts "*at (3)    : #{my_list.at(3)}"
