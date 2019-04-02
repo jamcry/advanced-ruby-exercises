c074a63bdbfd06b2034d5efe9f3e019fd0b4509d
jamcry <jamcry@hotmail.com>
2019-04-02 12:05:52 +0300

    Completed and refactored functions.

--- linked_lists.rb
+++ linked_lists.rb
@@ -6,14 +6,16 @@ class LinkedList
   
   def append(value)
     # Add a new node to the end of the list
-    # Append a new node to the end ( next node of tail) with specified value.
+    # Append a new node to the end ( next node of tail ) with specified value.
     tail.next_node = Node.new(value, nil)
   end
   
-  def preappend
+  def preappend(value)
     # add a new node to the start of the list
+    # Replace the head with new node with specified value
+    @head = Node.new(value, @head)
   end
-  
+
   def size
     # return the total number of nodes in the list
     current_node = @head
@@ -33,56 +35,75 @@ class LinkedList
   
   def tail
     # return the last node in the list
-    current_node = @head
-    while !current_node.next_node.nil?
-      current_node = current_node.next_node
-    end
-    current_node
+    self.each_with_index { |node, index| return node if index == (self.size - 1) }
   end
   
   def at(index)
     # return the node at the given index
-    current_node = @head
+  
     # return nil if index is out of range
     if index > (size - 1)
       nil
     else
-      index_count = 0
-      (0...index).each do |i|
-        current_node = current_node.next_node
-      end
-      current_node.value
+      self.each_with_index { |n,i| return n.value if i == index }
     end
   end
   
   def pop
     # remove the last element from the list
+
+    self.each do |node|
+      if node.next_node.next_node.nil?
+        last_value = node.next_node.value
+        node.next_node.value = nil
+        node.next_node = nil
+        return last_value
+      end
+    end
+    false
   end
   
-  def contains?(value)
+  def contains?(data)
     # return if value is in the list
+    self.each { |n| return true if n.value == data }
+    false
   end
   
   def find(data)
     # return the index of the node containing data or nil
-    #? can use (yield) in tail, inside while loop to use it
-    #? in find function.
-    #? tail {|val| val = data} break
+    self.each_with_index { |node, index| return index if node.value == data }
+    nil
   end
   
   def to_s
     # Parse LinkedList to string # => ( data ) -> ( data ) -> ( data ) -> nil
-    current_node = @head
-    parsed_list = ""
+    list_string = ""
     # Iterate until reaching the last (nil) node
-    until current_node.nil?
-      parsed_list += "( #{current_node.value} ) -> "
-      current_node = current_node.next_node
-    end
-    parsed_list + "nil"
+    self.each {|n| list_string << "( #{n.value} ) -> "}
+    list_string + "nil"
   end
   
+  def each
+    current_node = @head
+    while !current_node.nil?
+      # yield every node object
+      yield(current_node)
+      current_node = current_node.next_node
+    end
+  end
+
+  def each_with_index
+    current_node = @head
+    node_index = 0
+    while !current_node.nil?
+      yield(current_node, node_index)
+      current_node = current_node.next_node
+      node_index += 1
+    end
+  end
+
 end
+
 
 class Node
   attr_accessor :value, :next_node
@@ -106,3 +127,26 @@ puts "*Tail Value: #{my_list.tail.value}"
 puts "*at (1)    : #{my_list.at(1)}"
 puts "*at (2)    : #{my_list.at(2)}"
 puts "*at (3)    : #{my_list.at(3)}"
+print "*each puts:"
+my_list.each {|n| print " #{n.value}"}
+puts ""
+my_list.each_with_index {|n,i| puts "[#{i}] : ( #{n.value} )"}
+
+puts "Contains 4: #{my_list.contains?(4)}"
+puts "Contains 24: #{my_list.contains?(24)}"
+puts "Find 4 : #{my_list.find(4)}"
+puts "Find 7 : #{my_list.find(7)}"
+
+puts "Preappend 452..."
+my_list.preappend(452)
+puts my_list.to_s
+
+puts "Preappend 42..."
+my_list.preappend(42)
+puts my_list.to_s
+
+puts "Pop..."
+puts my_list.pop
+
+puts my_list.to_s
+puts my_list.size
