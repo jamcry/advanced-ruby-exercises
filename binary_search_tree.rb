001693a8d86c161ad4eb16102a321214615670f1
jamcry <jamcry@hotmail.com>
2019-04-04 11:48:32 +0300

    Added Breadth-First Search function.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -38,6 +38,23 @@ class Node
     end
   end
 
+  def breadth_first_search(target)
+    # create an array that acts like a queue
+    queue = [self]
+
+    # search until there are no nodes left
+    while queue.length > 0
+        # pop the first element and assign to the node variable
+        node = queue.shift
+        # stop searching and return if the target value is found
+        return node if node.value == target
+        # add the children of the current node to the queue if any
+        queue << node.children[:left] if !node.children[:left].nil?
+        queue << node.children[:right] if !node.children[:right].nil?
+    end
+    nil
+  end
+
   def left
     self.children[:left]
   end
@@ -50,13 +67,17 @@ end
 wiki = Node.new()
 wiki.build_tree([8,3,10,1,6,14,4,7,13])
 
-puts "Input Array: [8,3,10,1,6,14,4,7,13]".center(45,"=")
+puts "Input Array: [8,3,10,1,6,14,4,7,13]".center(45,"="), ""
 puts "Binary Search Tree".upcase.center(45,"-")
 puts "     #{wiki.value}"
 puts "   /   \\"
 puts "  #{wiki.left.value}    #{wiki.right.value}"
-puts " / \\     \\"
+puts " / \\  /  \\"
 puts "#{wiki.left.left.value}    #{wiki.left.right.value}   #{wiki.right.right.value}"
-puts "   / \\     \\"
+puts "   / \\  /  \\"
 puts "  #{wiki.left.right.left.value}    #{wiki.left.right.right.value}   #{wiki.right.right.left.value}"
 # Needs a better way to print the tree.
+puts "","Testing Search Algorithms".center(45,"-")
+puts "> Breadth-First Search (3): #{wiki.breadth_first_search(3)}"
+puts "> Breadth-First Search (13): #{wiki.breadth_first_search(13)}"
+puts "> Breadth-First Search (45): #{wiki.breadth_first_search(45)}"
